214589adf3e58f420e7a45a6513bfb2c7f0822d4
jaaack233 <jackchen990227@gmail.com>
2021-02-26 14:56:58 +0800

    refine

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-exports.view = function(req, res){
-  res.render('ticket');
-=======
 exports.view = function(req, res){
     res.render('ticket');
->>>>>>> 25a32ac3d9f4ae30f01b377f3a97940e8b1b272e
 };
