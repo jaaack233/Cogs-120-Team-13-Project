64aca51886bff99f23bbea988b19fde5720fe21c
jaaack233 <jackchen990227@gmail.com>
2021-03-04 20:52:49 +0800

    refine login

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,17 +1,22 @@
+const e = require("express");
 const data = require("../data.json");
 
 exports.verify = function (req, res, next) {
 
-    const username = req.body.username;
-    const password = req.body.password;
-    var pass = data[username].password;
-
-    console.log(username);
-
-    if (password === pass) {
-        console.log("You have successfully logged in.");
-        return next();
-    } else {
+    if (req.body.username == null || req.body.password == null || !data[req.body.username] ) {
         res.redirect('/login');
+    } else {
+        const username = req.body.username;
+        const password = req.body.password;
+        var pass = data[username].password;
+
+        if (password === pass) {
+            console.log("You have successfully logged in.");
+            return next();
+        } else {
+            res.redirect('/login');
+        }
     }
+
+
 }
